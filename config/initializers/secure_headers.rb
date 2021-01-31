610bfdcb79ed041dd563103d88a9d338f3f6f899
Ying Huang <huangyingw@gmail.com>
2021-01-31 14:12:47 -0800

    n

--- config/initializers/secure_headers.rb
+++ config/initializers/secure_headers.rb
@@ -16,8 +16,9 @@ SecureHeaders::Configuration.default do |config|
     img_src:         %w['self' data: blob: *],
     media_src:       %w[https:],
     script_src:      %w['self' blob: 'unsafe-eval' platform.twitter.com cdn.syndication.twimg.com widgets.flickr.com
-                        embedr.flickr.com platform.instagram.com 'unsafe-inline'],
-    style_src:       %w['self' 'unsafe-inline' platform.twitter.com *.twimg.com]
+                        embedr.flickr.com www.instagram.com 'unsafe-inline'],
+    style_src:       %w['self' 'unsafe-inline' platform.twitter.com *.twimg.com],
+    manifest_src:    %w['self']
   }
   # rubocop:enable Lint/PercentStringArray
 
